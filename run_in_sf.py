42db64a04d6f1781d21cfea0e1074aa4684da6d1
Denis Zvezdov <denisz@cqg.com>
2013-02-08 16:34:46 +0200

    clean-up

--- run_in_sf.py
+++ run_in_sf.py
@@ -1,14 +1,13 @@
 import sublime, sublime_plugin
-from subprocess import call
 import os
+from subprocess import call
 from os.path import basename, dirname
-import re
 
 class RunInSfCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         file_path = self.view.file_name()
-        file_name = os.path.basename(file_path)
-        suite_name = os.path.basename(os.path.dirname(file_path))
+        file_name = basename(file_path)
+        suite_name = basename(dirname(file_path))
         system_name = basename(file_path[:file_path.rfind("\SystemAreas")])
         path_to_sf = file_path[:file_path.rfind("\Scripts")]
         config_path = os.path.join(path_to_sf, 'Scripts', system_name, 'RunConfigs', suite_name + ".conf")
@@ -18,6 +17,3 @@ class RunInSfCommand(sublime_plugin.TextCommand):
         os.chdir(path_to_sf)
         call([r'c:\windows\system32\cmd.exe', '/K', launcher_path, '-c', config_path,
              '-o', file_name])
-
-
-
